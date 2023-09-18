fd187130e899bf39ac2722efd58b3cc052388e0e
onionmccabbage <onionmccabbage@live.com>
2023-09-18 16:29:14 +0100

    end of day 1

--- new_match.py
+++ new_match.py
@@ -2,14 +2,18 @@
 
 def main():
     lang = input('What prog language do you like?: ')
+    lang = []
     match lang:
+        # we can match agaisnt ANYTHING of ANY data type
         case 'Javascript':
             print('You can develop web stuff')
         case 'Python':
             print('You can become a data scientist')
         case 'Java':
             print('You can be a back-end developer')
-        case _:
+        case []:
+            print('That is an empty list')
+        case _: # catches any case we have not yet dealt with
             print('Whatever language, you can solve problems')
 
 if __name__ == '__main__':
