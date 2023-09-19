3ba38734610c83e3211f027b5ef5abb5a50d5708
onionmccabbage <onionmccabbage@live.com>
2023-09-19 10:59:44 +0100

    coffee break day 2

---
+++ exception_handling/using_try.py
@@ -0,0 +1,9 @@
+# Sometimes there is a risk things might go wrong
+# Python will raise an exception
+# We can choose what to do with exceptions
+
+def askUser():
+    '''ask the user to enter a number'''
+
+if __name__ == '__main__':
+    pass
