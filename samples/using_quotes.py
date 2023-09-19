b4f7ff54d09942f26740db83964673f44cf5679d
onionmccabbage <onionmccabbage@live.com>
2023-09-19 09:44:30 +0100

    using quotes

---
+++ samples/using_quotes.py
@@ -0,0 +1,11 @@
+# There is no difference between using single or double quotes
+# single quotes let us do this...
+print('the duck is called "Feathers" McGraw')
+# and double quotes let us do this...
+print("the duck is called 'Feathers' McGraw")
+
+# we also have triple quotes in Python
+def fn():
+    """here is a docstring
+    it lets us write over several lines
+    like this"""
