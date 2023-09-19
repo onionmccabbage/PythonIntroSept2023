94c1c7fde6b184f0a86eda6686ce4ff6d696a115
onionmccabbage <onionmccabbage@live.com>
2023-09-19 11:34:41 +0100

    try-except

--- exception_handling/using_try.py
+++ exception_handling/using_try.py
@@ -4,6 +4,19 @@
 
 def askUser():
     '''ask the user to enter a number'''
+    # remember every 'input' is ALWAYS a string
+    age = input('Please enter a number: ')
+    # we need to store this as a numeric value
+    # this will fail if the value is not numeric
+    try:
+        age_int = int(age)
+        print(age_int)
+    except ValueError as e: # handle specific kind of error
+        print (f'Value Error: {e}')
+    except Exception as e: # handle any other kind of error
+        print(f'Generic Exception: {e}')
+    finally: # optional
+        print('The finally block will always run, whether or not there is an error')
 
 if __name__ == '__main__':
-    pass
+    askUser() # invoke our function (run it)
