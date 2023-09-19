0ec97173421a35519fea3b36c85a7c810050dea6
onionmccabbage <onionmccabbage@live.com>
2023-09-19 12:01:22 +0100

    read simple text file

--- fifo/file_access.py
+++ fifo/file_access.py
@@ -4,14 +4,21 @@ def writeToFile(data):
     '''Take some data and write it to a text file'''
     # we need a file access object
     # NB if the file does not exist, Python will ask the OS to create it
-    fout = open('data.txt', 'at') # 'a' means 'append' and 't' means 'text
+    fout = open('data.txt', 'at') # 'w' means (over)write 'a' means 'append' and 't' means 'text
     # we can simply print some text top our file
     print(data, file=fout)
     # its a good idea to close the file access object when we are done
     fout.close()
 
 def readFromFile():
-    pass
+    '''use a file access object to open and red a text file'''
+    fin = open('data.txt', 'rt') # 'r' means read 't' means text
+    r = fin.readlines()
+    print(r)
 
 if __name__ == '__main__':
-    writeToFile('that was easy')
+    # ask the user to enter a name
+    n = input('Please enter a name: ')
+    # store that name in a text file
+    writeToFile(n)
+    readFromFile()
