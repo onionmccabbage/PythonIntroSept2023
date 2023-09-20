a12fede2e8ec6a57da3b58d25e4ebf0ce16284df
onionmccabbage <onionmccabbage@live.com>
2023-09-20 12:05:35 +0100

    sample Triangle class

--- using_classes/class_triangle.py
+++ using_classes/class_triangle.py
@@ -14,7 +14,7 @@ class Triangle(Shape):
             self.__x = new_x
         else:
             self.__x = 3 # set x to a safe default value
-    @property
+    @property # @property is entirely optional. It has been available since Py 3.5
     def y(self): # this is the getter method
         return self.__y
     @y.setter
@@ -22,10 +22,15 @@ class Triangle(Shape):
         if type(new_y)==float or type(new_y)==int:
             self.__y = new_y
         else:
-            self.__y = 4 # set x to a safe default value    
+            self.__y = 4 # set x to a safe default value  
+    # we can derive the hypotenuse of a right-angle triangle  
+    @property # we ONLY declare a getter (no setter)
+    def hypot(self): # this is now a read-only property
+        h = (self.x**2 + self.y**2)**0.5
+        return h
 
 if __name__ == '__main__':
     t1 = Triangle(3, 4, 5)
-    t1.x = 'oh dear' # here we directly access the x property of the Tyiange instace
-    t1.y = 3.6
-    print(f'{t1.num_sides} sides {t1.x} by {t1.y}')
+    t1.x = 'oh dear' # here we try to directly access the x property of the Triange instace
+    t1.y = 4 # this uses the y setter method to set __y
+    print(f'{t1.num_sides} sides {t1.x} by {t1.y} has h={t1.hypot}')
