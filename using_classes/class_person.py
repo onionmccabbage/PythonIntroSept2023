9587f2d8fadb7cf55f7a0792af94ee71d433ee4c
onionmccabbage <onionmccabbage@live.com>
2023-09-20 10:16:11 +0100

    validating class properties

--- using_classes/class_person.py
+++ using_classes/class_person.py
@@ -6,21 +6,27 @@
 class Person:
     '''This class enclapsulates a person
     we store name, age and language'''
-    def __init__(self, n, a, l): # every runction in a class MUST take 'self' as an argument
+    def __init__(self, n, a, l): # every function in a class MUST take 'self' as an argument
         print('a Person instance is being created')
         self.n = n
         self.a = a
         self.l = l
     def setName(self, new_name):
-        pass
+        if len(new_name)>0 and type(new_name)==str:
+            self.n = new_name
+        else:
+            self.n = 'default'
 
 if __name__ == '__main__':
     # we can create instances of our class
     Arianne = Person('Arianne', 42, 'Python') # every time we make an instance it will run __init__
     Betty   = Person('Betty', 35, 'Java')
     Con     = Person('Con', 23, 'Python')
+    Arianne.setName('')
+        
     # we can access any properties like this (called 'dot notation')
     print( Arianne.n, Arianne.a, Arianne.l )
+
 
    
 
